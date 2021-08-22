94b6162a16151b95af4244029908ae0b8d614ed7
Alejandro Monge <amonge_rey@hotmail.com>
2021-08-22 15:10:01 -0500

    Add user routes to server

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import { PORT, MONGODB_URL } from "./config.js";
 
+import userRoute from "./routes/userRoute";
+
 mongoose
   .connect(MONGODB_URL, {
     useNewUrlParser: true,
@@ -11,6 +13,8 @@ mongoose
   .catch((err) => console.log(err.reason));
 
 const app = express();
+
+app.use("/api/user/", userRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

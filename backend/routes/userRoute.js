97bbb4482d1953b3e213bf8d7b82ee048937e347
Alejandro Monge <amonge_rey@hotmail.com>
2021-08-22 16:29:23 -0500

    Basic endpoints to users and Step #1

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,18 @@ import User from "../models/userModel.js";
 
 const router = express.Router();
 
+router.get("/all", async (req, res) => {
+  const users = await User.find();
+  if (users) {
+    var allUsers = [];
+    users.forEach((user) => {
+      const { __v, ...rest } = user._doc;
+      allUsers.push({ ...rest });
+    });
+    res.status(200).send(allUsers);
+  }
+});
+
 router.post("/add", async (req, res) => {
   const user = new User({
     ...req.body,
@@ -22,6 +34,46 @@ router.post("/add", async (req, res) => {
       message: `New user '${newUser.name}' with id: '${newUser._id}' was registered correctly`,
     });
     return;
+  }
+});
+
+router.put("/modify/:id", async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id });
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    user.status = req.body.status || user.status;
+    const updatedUser = await user.save().catch(({ code, keyValue }) => {
+      if (code === 11000) {
+        res
+          .status(409)
+          .send({ error: true, message: "Duplicate key", keyValue });
+        return;
+      }
+      res.status(500).send({
+        error: true,
+        message: "Something went wrong",
+      });
+    });
+    if (updatedUser) {
+      res.status(200).send({
+        message: "User updated",
+        user: { ...updatedUser },
+      });
+    }
+  } else {
+    res.send({ message: "User not found", error: true });
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  const deleteUser = await User.findById(req.params.id);
+  if (deleteUser) {
+    await deleteUser.remove();
+    res.send({ message: "User deleted" });
+  } else {
+    res.send({ message: "User not found", error: true });
   }
 });
 
